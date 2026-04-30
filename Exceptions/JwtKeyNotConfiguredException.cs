976fd1f6c512e4eb06f0f3510b9b7abb4293cdac
Leonardo Samuel <leonardosmsilva2004@gmail.com>
2026-04-30 11:30:40 -0300

    feat: adicionando exceção para JWT KEY não configurada.

---
+++ Exceptions/JwtKeyNotConfiguredException.cs
@@ -0,0 +1,7 @@
+﻿namespace ApsMartChat.Exceptions;
+
+public class JwtKeyNotConfiguredException : Exception
+{
+    public JwtKeyNotConfiguredException (string msg) : base(msg) { }
+    public JwtKeyNotConfiguredException() : base("JWT Key não configurada") { }
+}
