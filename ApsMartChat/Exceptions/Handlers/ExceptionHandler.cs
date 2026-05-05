e39c2714e28fa4b3ad682c7f4bce4661b3a1470e
Leonardo Samuel <leonardosmsilva2004@gmail.com>
2026-05-05 19:59:55 -0300

    feat: adicionando a nova exceção customizada para numero max de arquivos no upload ultrapassado.

--- ApsMartChat/Exceptions/Handlers/ExceptionHandler.cs
+++ ApsMartChat/Exceptions/Handlers/ExceptionHandler.cs
@@ -65,6 +65,10 @@ public class ExceptionHandler
         {
             await Handle(context, 400, ex.Message);
         }
+        catch (MaxFilesExceededException ex)
+        {
+            await Handle(context, 400, ex.Message);
+        }
 
         // sempre manter em ultimo. por ser a generic exception, cobre tudo
         catch (Exception ex)
---
+++ ApsMartChat/Exceptions/MaxFilesExceededException.cs
@@ -0,0 +1,7 @@
+﻿namespace ApsMartChat.Exceptions;
+
+public class MaxFilesExceededException : Exception
+{
+    public MaxFilesExceededException() : base("O tipo do entrada é inválida."){}
+    public MaxFilesExceededException(string msg) : base(msg){}
+}
